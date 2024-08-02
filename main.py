558e4c093caf3e2b4796f938d21f612a24f2746e
bruno7sales <bruno07sales@gmail.com>
2024-08-02 17:54:14 -0300

    version 1

---
+++ main.py
@@ -0,0 +1,6 @@
+# Entrada de Daods
+estados = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia','Roraima', 'Santa Catarina', 'São Paulo','Sergipe','Tocantins']
+
+# Saida de Dados
+for i in  range(27):
+    print(f'{i + 1}° nome: {estados[i]}. ')
